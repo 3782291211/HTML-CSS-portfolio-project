ff3ae062721d1c8e1b6c0fa99c5d8df99fb49615
3782291211 <Laklouk98@hotmail.com>
2022-12-28 17:27:51 +0000

    adding media queries for tablet and smartphone viewports

---
+++ README.md
@@ -0,0 +1,36 @@
+# Responsive web design with **CSS & HTML**
+
+**[Link to my live website](https://3782291211.github.io/HTML-CSS-portfolio-project/)**
+
+_Created on December 2022._
+
+Hello, and welcome! This repository holds all the files for a CSS & HTML project that I have completed. Visit the link above to view and explore my website, or feel free to clone and run the repository in your local environment.
+
+---
+
+## Webpage guide
+- In the home page, hover over each character card to view additional information.
+- The second page contains a glossary of terms. Hover over each word to reveal its definition.
+- The third page contains a questionnaire which is constructed recursively using JavaScript.
+
+---
+
+## Proficiency overview
+
+Here are some of the skills I've demonstrated by completing this project:
+- Heavy emphasis on **responsive web design** through extensive use of media queries, relative units and dynamic structures, resulting in a finely-tuned layout that is very responsive to changes in viewport width.
+- **Interactive CSS** design, through the use of animations, pseudo-elements and pseudo-classes.
+- **DOM manipulation** using JavaScript to provide user interactivity.
+- Use of developer tools.
+   - Responsiveness was tested rigorously on a wide range of smartphone and tablet models (listed within DevTools) with varying display dimesions.
+   - I also used rendering tools and performance analytics to ensure good performance even under severed hardware/network limitations.
+
+---
+
+## Screenshots
+
+<img src="Screenshot-page1.png" width="400px"/>
+
+<img src="Screenshot-page2.png" width="400px"/>
+
+<img src="Screenshot-page3.png" width="400px"/>
--- script.js
+++ script.js
@@ -1,148 +1,199 @@
 const mainDiv = document.querySelector(".main-div");
 const answers = [];
 const resultObj = {
-  chianna: 0,
-  crichton: 0,
-  scorpy: 0,
-  dargo: 0,
-  rygel: 0,
-  scorpy: 0,
-  aeryn: 0
+  Chianna: 0,
+  Crichton: 0,
+  Scorpy: 0,
+  Dargo: 0,
+  Rygel: 0,
+  Scorpy: 0,
+  Aeryn: 0
   };
 
 const firstQuestion = `<div class="question1 intro"> <strong> Question 1 </strong> <br><br> You find yourself in a brand new place where you don't understand the local language. What do you do?<br><br>
 <div>
-<input class="q1" type="radio" name="question1" value="crichton">
+<input class="q1" type="radio" name="question1" value="Crichton">
 <label for="q1">Use as many obscure pop culture references as possible when talking to everyone.</label>
 </div>
 
 <div>
-<input class="q1" type="radio" name="question1" value="aeryn">
+<input class="q1" type="radio" name="question1" value="Aeryn">
 <label for="q1">Distrust everyone and always remain grumpy.</label>
 </div>
 
 <div>
-<input class="q1" type="radio" name="question1" value="scorpy">
+<input class="q1" type="radio" name="question1" value="Scorpy">
 <label for="q1">Slither your way into other people's good graces.</label>
 </div>
 
 <div>
-<input class="q1" type="radio" name="question1" value="chianna">
+<input class="q1" type="radio" name="question1" value="Chianna">
 <label for="q1">Try and seduce as many of the locals as possible (or get banished for your efforts).</label>
 </div>
 </div>`;
 
 const secondQuestion = `<div class="question2 intro"> <strong>Question 2</strong> <br><br> You haven't eaten in a long time and you are in the mood for a special treat. What do you go for?<br><br>
 <div>
-<input class="q2" type="radio" name="question2" value="rygel">
+<input class="q2" type="radio" name="question2" value="Rygel">
 <label for="q2">Marjools.</label>
 </div>
 
 <div>
-<input class="q2" type="radio" name="question2" value="crichton">
+<input class="q2" type="radio" name="question2" value="Crichton">
 <label for="q2">Chocolate.</label>
 </div>
 
 <div>
-<input class="q2" type="radio" name="question2" value="chianna">
+<input class="q2" type="radio" name="question2" value="Chianna">
 <label for="q2">Raslak and fellip nectar.</label>
 </div>
 
 <div>
-<input class="q2" type="radio" name="question2" value="aeryn">
+<input class="q2" type="radio" name="question2" value="Aeryn">
 <label for="q2">Tannot root.</label>
 </div>
 </div>`;
 
 const thirdQuestion = `<div class="question3 intro"> <strong>Question 3</strong> <br><br> You're bored and looking for something fun to do. Where do you go?<br><br>
 <div>
-<input class="q3" type="radio" name="question3" value="rygel">
+<input class="q3" type="radio" name="question3" value="Rygel">
 <label for="q3">Take a very long double-nap.</label>
 </div>
 
 <div>
-<input class="q3" type="radio" name="question3" value="crichton">
+<input class="q3" type="radio" name="question3" value="Crichton">
 <label for="q3">Wander through the corridors, singing and talking to yourself.</label>
 </div>
 
 <div>
-<input class="q3" type="radio" name="question3" value="chianna">
+<input class="q3" type="radio" name="question3" value="Chianna">
 <label for="q3">Head off to the nearest party.</label>
 </div>
 
 <div>
-<input class="q3" type="radio" name="question3" value="dargo">
+<input class="q3" type="radio" name="question3" value="Dargo">
 <label for="q3">Start a fight.</label>
 </div>
 </div>`;
 
 const fourthQuestion = `<div class="question4 intro"> <strong>Question 4</strong> <br><br> You happen upon a large sum of unclaimed money. What do you do?<br><br>
 <div>
-<input class="q4" type="radio" name="question4" value="rygel">
+<input class="q4" type="radio" name="question4" value="Rygel">
 <label for="q4">Hoard all the money and tell no one.</label>
 </div>
 
 <div>
-<input class="q4" type="radio" name="question4" value="crichton">
+<input class="q4" type="radio" name="question4" value="Crichton">
 <label for="q4">Try to convince all your friends to spend the money on something you need, while making them think it was their idea.</label>
 </div>
 
 <div>
-<input class="q4" type="radio" name="question4" value="aeryn">
+<input class="q4" type="radio" name="question4" value="Aeryn">
 <label for="q4">Money? You haven't dealt in anything as common as money in a long time.</label>
 </div>
 
 <div>
-<input class="q4" type="radio" name="question4" value="dargo">
+<input class="q4" type="radio" name="question4" value="Dargo">
 <label for="q4">You don't care about money. You only care about fighting.</label>
 </div>
 </div>`;
 
-const fifthQuestion = `<div class="question5"> <strong>Question 5</strong> <br><br> How would others describe your atittude towards authority?<br><br>
+const fifthQuestion = `<div class="question5 intro"> <strong>Question 5</strong> <br><br> How would others describe your atittude towards authority?<br><br>
 <div>
-<input class="q5" type="radio" name="question4" value="rygel">
+<input class="q5" type="radio" name="question4" value="Rygel">
 <label for="q5">Distrustful.</label>
 </div>
 
 <div>
-<input class="q5" type="radio" name="question4" value="aeryn">
+<input class="q5" type="radio" name="question4" value="Aeryn">
 <label for="q5">Indoctrinated.</label>
 </div>
 
 <div>
-<input class="q5" type="radio" name="question4" value="dargo">
+<input class="q5" type="radio" name="question4" value="Dargo">
 <label for="q5">Aggressive.</label>
 </div>
 
 <div>
-<input class="q5" type="radio" name="question4" value="chianna">
+<input class="q5" type="radio" name="question4" value="Chianna">
 <label for="q5">Perturbed.</label>
 </div>
 </div>`;
 
-const finishMsg = `<div class="question6"> And you're all done.</div>`;
+const sixthQuestion = `<div class="question6 intro"> <strong>Question 6</strong> <br><br> How do you tend to deal with difficult tasks?<br><br>
+<div>
+<input class="q6" type="radio" name="question4" value="Scorpy">
+<label for="q6">You analyse the details and take your time formulating a solution.</label>
+</div>
+
+<div>
+<input class="q6" type="radio" name="question4" value="Aeryn">
+<label for="q6">You stick to what you know best - rules and procedures.</label>
+</div>
+
+<div>
+<input class="q6" type="radio" name="question4" value="Dargo">
+<label for="q6">With speed and precision.</label>
+</div>
+
+<div>
+<input class="q6" type="radio" name="question4" value="Chianna">
+<label for="q6">You seek advice from others and ignore all of it because you know better.</label>
+</div>
+</div>`;
+
+const seventhQuestion = `<div class="question7 intro"> <strong>Question 7</strong> <br><br>You've just finished a long and exhausting day. What do you do to decompress and relax?<br><br>
+<div>
+<input class="q7" type="radio" name="question4" value="Crichton">
+<label for="q7">Dance, wave your arms, run around and sing your favourite songs loudly.</label>
+</div>
+
+<div>
+<input class="q7" type="radio" name="question4" value="Aeryn">
+<label for="q7">Do some chores around the house.</label>
+</div>
+
+<div>
+<input class="q7" type="radio" name="question4" value="Dargo">
+<label for="q7">Complete a high-intensity session of exercise or strength training.</label>
+</div>
+
+<div>
+<input class="q7" type="radio" name="question4" value="Chianna">
+<label for="q7">Chat with your friends.</label>
+</div>
+</div>`;
 
 const nextButton = document.querySelector("#select-question");
 const submitButton = document.querySelector("#submit-answer");
 const submitLabel = document.getElementById("submit-answer-label");
 const introText = document.getElementById('intro-text');
-const errorDiv = document.getElementById('error-div');
+const error = document.getElementById('error');
+const characterCards = document.querySelectorAll('.image-description');
 
 function showQuestion(questionNumber = 1) {
-  if (questionNumber === 6) {
-    mainDiv.innerHTML = finishMsg;
+  if (questionNumber === 8) {
     submitButton.remove();
     nextButton.remove();
-
     answers.forEach(m => resultObj[m]++);
     const scores = Object.entries(resultObj).sort((a,b) => b[1] - a[1]);
+    
     if (scores[0][1] !== scores [1][1]) {
-       document.body.append(`<div class="red-box">You are most like ${scores[0][0]}</div>`);
-    } else if (scores[0][1] === scores[1][1]) {
-      document.body.append(`<div class="red-box">You are most like ${scores[0][0]} and ${scores[1][0]}</div>`);
-    }
-   console.log(scores);
-    return;
+      submitLabel.innerText += ` You are most like ${scores[0][0]}.`;
+        characterCards.forEach(character => {
+          if (character.id === scores[0][0]) {
+            character.style.display = 'block';
+          }
+        });
+       
+      } else if (scores[0][1] === scores[1][1]) {
+        submitLabel.innerText += ` You are most like ${scores[0][0]} and ${scores[1][0]}.`;
+          characterCards.forEach(character => {
+            if (character.id === scores[0][0] || character.id === scores[1][0]) {
+              character.style.display = 'block';
+            }
+         });
+      }
   }
   const questions = [
     firstQuestion,
@@ -150,8 +201,11 @@ function showQuestion(questionNumber = 1) {
     thirdQuestion,
     fourthQuestion,
     fifthQuestion,
+    sixthQuestion,
+    seventhQuestion
   ];
-  nextButton.addEventListener("click", (event) => {
+
+  nextButton.addEventListener("click", () => {
     nextButton.style.display = 'none';
     submitLabel.style.display = 'none';
     introText.remove();
@@ -165,31 +219,30 @@ function showQuestion(questionNumber = 1) {
    
   });
 
-  submitButton.addEventListener("click", (event) => {
+  submitButton.addEventListener("click", () => {
+    if (submitButton.style.display !== 'none') {
+      error.style.display = 'block';
+    } 
     const options = document.querySelectorAll(`.q${questionNumber}`);
-    options.forEach((input, i) => {
+
+    options.forEach(input => {
       if (input.checked) {
-        errorDiv.innerHTML = '';
         nextButton.style.display = 'block';
+        error.style.display = 'none';
         submitButton.style.display = 'none';
         answers.push(input.value);
      
         const message =
-          questionNumber < 5
+          questionNumber < 7
             ? `Answer submitted. Proceed to question ${questionNumber + 1}.`
-            : "Click the button to see your results.";
+            : 'Your results are in!';
 
         submitLabel.style.display = 'block';
-        submitLabel.innerText = message;
+        submitLabel.innerHTML = message;
         nextButton.innerText = "Next question.";
         document.querySelector(`.question${questionNumber}`).remove();
         questionNumber++;
-       
-
         showQuestion(questionNumber);
-      } else {
-        errorDiv.innerHTML = '<p id="error" class="intro red-intro"> Please select an option</p>';
-       // setTimeout(() => errorDiv.innerHTML = '', 4000)
       }
     });
     
@@ -197,7 +250,3 @@ function showQuestion(questionNumber = 1) {
 }
 
 showQuestion();
-
-
-
-
